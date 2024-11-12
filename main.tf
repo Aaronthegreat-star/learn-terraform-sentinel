60a19fafcaf7635ef80fded791082c4f0c4b7c55
Onyeani Njoku victor <126138357+Aaronthegreat-star@users.noreply.github.com>
2024-11-12 23:19:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
