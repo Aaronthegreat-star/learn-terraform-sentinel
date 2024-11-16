4dff915c8bb1d5b1baaddcc47efc93703d846829
Onyeani Njoku victor <126138357+Aaronthegreat-star@users.noreply.github.com>
2024-11-16 22:57:51 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,12 +13,3 @@ variable "instance_name" {
   default = "Provisioned by Terraform"
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type      = string
-  sensitive = true
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type      = string
-  sensitive = true
-}
